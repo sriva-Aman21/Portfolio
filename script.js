b8e7a06e1047a3259cf473c01253cc86365c611e
sriva-Aman21 <aman21sriva@gmail.com>
2026-04-08 12:39:45 +0530

    Initial commit - portfolio website

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Science Mentor", "Sports Data Analyst", "Visualisation Specialist"],
+        strings: ["Aspiring Data Analyst", "Data Enthusiast", "Visualization Specialist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Freelancer", "Blogger", "Instructor"],
+        strings: ["Data Analyst", "Problem Solver", "Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
